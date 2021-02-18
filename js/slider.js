5b6fba4a8cca3c31069dca529c1a78c2f95f9680
VelichkoRoman <78850665+VelichkoRoman@users.noreply.github.com>
2021-02-18 12:36:04 +0300

    feat: ready slider non-adaptive and some codefix's

--- js/slider.js
+++ js/slider.js
@@ -2,17 +2,17 @@ let offset=0; //смещение от левого края
 const sliderLine=document.querySelector('.slider-line');
 
 document.querySelector('.slider-next').addEventListener('click',function(){
-    offset=offset+274; 
-    if (offset>822){
+    offset=offset+360; 
+    if (offset>1080){
         offset=0;
     }
     sliderLine.style.left=-offset+'px';
 });
 
 document.querySelector('.slider-prev').addEventListener('click',function(){
-    offset=offset-274; 
+    offset=offset-360; 
     if (offset<0){
-        offset=822;
+        offset=1080;
     }
     sliderLine.style.left=-offset+'px';
 });
